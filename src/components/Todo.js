543eb6164dad3682516ebd3538b0e5b80b4d690e
Didem Cakir <didemydn@gmail.com>
2023-10-21 22:46:21 +0200

    done

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -6,12 +6,24 @@ import {TiEdit} from 'react-icons/ti'
 
 
 
-function Todo({todos, completeTodo}) {
+function Todo({todos, completeTodo, removeTodo, updateTodo}) {
 
     const [edit, setEdit] = useState({
         id:null,
         value: ""
-    })
+    });
+
+    const submitUpdate= value => {
+        updateTodo(edit.id, value)
+        setEdit({
+            id: null,
+            value: ""
+        })
+    }
+
+    if(edit.id) {
+        return <TodoForm edit = {edit} onSubmit={submitUpdate}/>
+    }
 
   return todos.map((todo, index) => (
     <div 
@@ -22,8 +34,12 @@ function Todo({todos, completeTodo}) {
             {todo.text}
         </div>
         <div className="icons">
-            <RiCloseCircleLine />
-            <TiEdit />
+            <RiCloseCircleLine 
+            onClick={() => removeTodo(todo.id)}
+            className="delete-icon"/>
+            <TiEdit 
+            onClick={() => setEdit({id: todo.id, value:todo.text})}
+            className="edit-icon"/>
         </div>
         </div>
   ));
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -14,7 +14,21 @@ function TodoList() {
         const newTodos = [todo, ...todos]
 
         setTodos(newTodos)
-    }
+    };
+
+    const updateTodo = (todoId, newValue) => {
+        if(!newValue.text || /^\s*$/.test(newValue.text)){
+            return
+        }
+
+        setTodos(prev => prev.map(item => (item.id === todoId ? newValue: item)))
+    };
+
+    const removeTodo = id => {
+        const removeArr = [...todos].filter(todo => todo.id !== id)
+
+        setTodos(removeArr);
+    };
 
     const completeTodo = id => {
         let updatedTodos = todos.map(todo => {
@@ -31,7 +45,7 @@ function TodoList() {
         <h1>What is the plan for today?</h1>
         <TodoForm onSubmit={addTodo}/>
         <Todo 
-        todos = {todos} completeTodo = {completeTodo}/>
+        todos = {todos} completeTodo = {completeTodo} removeTodo={removeTodo} updateTodo = {updateTodo}/>
     </div>
   );
 }
